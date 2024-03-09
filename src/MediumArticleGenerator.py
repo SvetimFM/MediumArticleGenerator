3ef7ef7bb21475f020b2bbeabbe473630fc60aff
aleks A <aleks.allen@engineer.com>
2024-03-09 01:24:19 -0800

    full article generation, GPT3.5 instruct, updated prompts, some cleanup

--- src/MediumArticleGenerator.py
+++ src/MediumArticleGenerator.py
@@ -1,30 +1,57 @@
 from enum import Enum
 
 from data.apikey import API_KEY
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 
-TITLE_GENERATOR_TEMPLATE = "Article Title: A succinct yet powerful title encapsulating the essence of the article " \
-                           "on {topic}. output should be only in {output_language}. if it cannot be produced in that " \
-                           "language, produce output stating why output could not be produced. Try hard to produce" \
-                           "output in the {output_language}. The title should be a balanced fusion of creativity " \
-                           "and clarity, making use of the provided inputs to directly address the interests and" \
-                           " needs of the target audience. Do not provide translation."
+
+with open('prompts/medium_article_title_generator_prompt.txt', 'r', encoding='utf-8') as file:
+    TITLE_GENERATOR_TEMPLATE = file.read()
+
+with open('prompts/medium_article_generator_prompt.txt', 'r', encoding='utf-8') as file:
+    ARTICLE_GENERATOR_TEMPLATE = file.read()
+
+GPT4 = 'gpt-4'
+GPT3 = "gpt-3.5-turbo-instruct"
 
 
-def get_llm():
-    return OpenAI(temperature=0.8, openai_api_key=API_KEY)
+def get_llm(llm_name: str = GPT3):
+    return OpenAI(model_name=llm_name, temperature=0.8, openai_api_key=API_KEY)
 
 
-def get_title_output_chain():
+def get_title_output_chain(llm: str):
     title_template = PromptTemplate(
-        input_variables=['topic', 'output_language'],
+        input_variables=['topic'],
         template=TITLE_GENERATOR_TEMPLATE
     )
-    return LLMChain(llm=get_llm(), prompt=title_template, verbose=True)
+    return LLMChain(llm=get_llm(llm), prompt=title_template, verbose=True)
 
 
-def get_title(values: dict[str, str]) -> str:
-    title_chain = get_title_output_chain()
-    return title_chain.run(values)
+def get_article_output_chain(llm: str):
+    article_template = PromptTemplate(
+        input_variables=['title'],
+        template=ARTICLE_GENERATOR_TEMPLATE
+    )
+    return LLMChain(llm=get_llm(llm), prompt=article_template, verbose=True)
+
+
+# clunky non-langchain approach
+def get_title(topic: str, llm: str = GPT3) -> str:
+    title_chain = get_title_output_chain(llm)
+    return title_chain.run(topic)
+
+
+def get_article(title: str, llm: str = GPT3) -> str:
+    article_chain = get_article_output_chain(llm)
+    return article_chain.run(title)
+
+
+# beautiful langchain approach
+def get_complete_output(value: str, llm: str = GPT3) -> str:
+    overall_chain = SimpleSequentialChain(
+        chains=[
+            get_title_output_chain(llm),
+            get_article_output_chain(llm)
+    ])
+    return overall_chain.run(value)
--- src/app.py
+++ src/app.py
@@ -1,22 +1,36 @@
 import streamlit as st
-from MediumArticleGenerator import get_llm, get_title
+from MediumArticleGenerator import get_llm, get_complete_output
 
-llm = get_llm()
 
 APPLET_TITLE = 'Medium Article Generator'
 TOPIC_BOX_TEXT = 'Enter the topic of the article'
 OUTPUT_LANGUAGE = 'Enter the output language'
 
+
 def user_form():
     st.title(APPLET_TITLE)
     topic = st.text_input(TOPIC_BOX_TEXT)
-    output_language = st.text_input(OUTPUT_LANGUAGE, 'English')
 
+# sequential chains are nice, but limit one to a single input/output variable.
     if topic:
-        response = get_title({'topic': topic,
-                              'output_language': output_language
-                              })
-        st.write(response)
+        article = get_complete_output(topic)
+        st.write(article)
+
+# example of how things would have to be done without langchain
+        # if topic:
+        #   article = get_complete_output(topic)
+        #   st.write(article)
+        #
+        #   article_title = get_title({
+        #       'topic': topic,
+        #   })
+        #
+        # article = get_article({
+        #     'title': article_title,
+        # })
+        #
+        # st.write(article_title)
+        # st.write(article)
 
 
 def main():
